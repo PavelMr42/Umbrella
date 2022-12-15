fff0fd9b0efd13db54beea23ff0eae8fcf00a9be
PavelMr42 <Pashagrigorev403@gmail.com>
2022-12-15 18:31:49 +0700

    Урок 10 - введение в js

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+function lines() {
+    alert(testName)
+}
+
+function NumberStrings() {
+    alert(888)
+}
+
+function lines1(lineResult) {
+    lineResult += ' - hello brothers'
+    alert(lineResult)
+}
+
+function lines2(lineResult) {
+    lineResult = lineResult + ' - hello brothers'
+    alert(lineResult)
+}
+
+function NumberConcantinations(ttt) {
+    numberResult = numberResult + '20'
+    alert(numberResult)
+}
+
+function AmountsNumber(numberResult) {
+    numberResult = numberResult + 20
+    alert(numberResult)
+}
+
+function AmountsNumber2(numberResult) {
+    numberResult = Number(numberResult) + 20
+    alert(numberResult)
+}
+
+function ObjectConsole() {
+    let object = {
+        "hi": 87,
+        "test": 47,
+        "bibi": 56,
+        "r2d2": 32,
+    }
+    console.log(ooo.hi)
+}
+
+function Array() {
+    let object = ['1', '2', '3',]
+    console.log(object)
+}
+
+function ArrayObjects() {
+    let object = [{'1': '1'}, ['2', '2'], '3',]
+    console.log(object)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    ArrayObjects()
+}, false);
+
